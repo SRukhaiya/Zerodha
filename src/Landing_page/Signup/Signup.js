61a54b4b1cca16ae3e76fbea28a003a00b0b5dc4
Sayed Rukhaiya <sayedrukhaiya777@gmail.com>
2025-07-17 08:31:31 +0530

    signup changed

--- src/Landing_page/Signup/Signup.js
+++ src/Landing_page/Signup/Signup.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Home from '../../../../dashboarddata/src/components/Home';
 function Signup() {
     return ( 
-        <h1>Signup</h1>
+        <div className="signup-container text-center m-5 p-5"  style={{marginTop:"10px",backgroundColor:"rgb(56,126,209)",borderRadius:"10px",color:"white"}}>
+        <h3 className="p-2">Signup</h3>
+        <form>
+            <div className="Username">
+                <label for="username" >Enter Username :</label>
+                <input type='text' id ="username" placeholder="enter username" required style={{width:"30",height:"40px",borderRadius:"5px",border:"1px solid #ccc",marginLeft:"10px",marginTop:"10px"}}/><br></br>
+                 <label for="password" style={{padding:"5px"}}>Enter Password :</label>
+                 <input type="password" id="password" placeholder='Enter Password' required style={{width:"30%",height:"40px",borderRadius:"5px",border:"1px solid #ccc",marginLeft:"10px",margin:"8px",marginTop:"10px"}}/><br></br>
+                 <button className="btn btn-primary">Sign Up</button>
+            </div>
+        </form>
+        </div>
      );
 }
 
